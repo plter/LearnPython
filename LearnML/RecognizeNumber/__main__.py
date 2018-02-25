2b6e9e2f76f003751bac562ca0eca50ca93ac301
yunp <xtiqin@163.com>
2018-02-25 22:25:20 +0800

    更新手写识别样本

--- LearnML/RecognizeNumber/__main__.py
+++ LearnML/RecognizeNumber/__main__.py
@@ -11,10 +11,14 @@ app = web.application(urls, globals())
 
 class hw:
     def GET(self):
-        json_str = web.input(_method='get')['photo_data']
-        photo_data = json.loads(json_str)
-        the_num = ml.recognize(photo_data)
-        result = the_num[0]
+        parameters = web.input(_method='get')
+        if parameters.has_key('photo_data'):
+            json_str = parameters['photo_data']
+            photo_data = json.loads(json_str)
+            the_num = ml.recognize(photo_data)
+            result = the_num[0]
+        else:
+            result = 'No parameters'
         return result
 
 
--- LearnML/RecognizeNumber/static/LoadHW/index.js
+++ LearnML/RecognizeNumber/static/LoadHW/index.js
@@ -4,16 +4,16 @@
 
         constructor() {
             this.IMAGE_URLS = [
-                ["0-01.jpg", "0-02.jpg", "0-03.jpg", "0-04.jpg", "0-05.jpg", "0-06.jpg", "0-07.jpg", "0-08.jpg", "0-09.jpg", "0-10.jpg", "0-01.png", "0-02.png", "0-03.png", "0-04.png", "0-05.png", "0-06.png", "0-07.png", "0-08.png", "0-09.png", "0-10.png"],
-                ["1-01.jpg", "1-02.jpg", "1-03.jpg", "1-04.jpg", "1-05.jpg", "1-06.jpg", "1-07.jpg", "1-08.jpg", "1-09.jpg", "1-10.jpg", "1-01.png", "1-02.png", "1-03.png", "1-04.png", "1-05.png", "1-06.png", "1-07.png", "1-08.png", "1-09.png", "1-10.png"],
-                ["2-01.jpg", "2-02.jpg", "2-03.jpg", "2-04.jpg", "2-05.jpg", "2-06.jpg", "2-07.jpg", "2-08.jpg", "2-09.jpg", "2-10.jpg", "2-01.png", "2-02.png", "2-03.png", "2-04.png", "2-05.png", "2-06.png", "2-07.png", "2-08.png", "2-09.png", "2-10.png"],
-                ["3-01.jpg", "3-02.jpg", "3-03.jpg", "3-04.jpg", "3-05.jpg", "3-06.jpg", "3-07.jpg", "3-08.jpg", "3-09.jpg", "3-10.jpg", "3-01.png", "3-02.png", "3-03.png", "3-04.png", "3-05.png", "3-06.png", "3-07.png", "3-08.png", "3-09.png", "3-10.png"],
-                ["4-01.jpg", "4-02.jpg", "4-03.jpg", "4-04.jpg", "4-05.jpg", "4-06.jpg", "4-07.jpg", "4-08.jpg", "4-09.jpg", "4-10.jpg", "4-01.png", "4-02.png", "4-03.png", "4-04.png", "4-05.png", "4-06.png", "4-07.png", "4-08.png", "4-09.png", "4-10.png"],
-                ["5-01.jpg", "5-02.jpg", "5-03.jpg", "5-04.jpg", "5-05.jpg", "5-06.jpg", "5-07.jpg", "5-08.jpg", "5-09.jpg", "5-10.jpg", "5-01.png", "5-02.png", "5-03.png", "5-04.png", "5-05.png", "5-06.png", "5-07.png", "5-08.png", "5-09.png", "5-10.png"],
-                ["6-01.jpg", "6-02.jpg", "6-03.jpg", "6-04.jpg", "6-05.jpg", "6-06.jpg", "6-07.jpg", "6-08.jpg", "6-09.jpg", "6-10.jpg", "6-01.png", "6-02.png", "6-03.png", "6-04.png", "6-05.png", "6-06.png", "6-07.png", "6-08.png", "6-09.png", "6-10.png"],
-                ["7-01.jpg", "7-02.jpg", "7-03.jpg", "7-04.jpg", "7-05.jpg", "7-06.jpg", "7-07.jpg", "7-08.jpg", "7-09.jpg", "7-10.jpg", "7-01.png", "7-02.png", "7-03.png", "7-04.png", "7-05.png", "7-06.png", "7-07.png", "7-08.png", "7-09.png", "7-10.png"],
-                ["8-01.jpg", "8-02.jpg", "8-03.jpg", "8-04.jpg", "8-05.jpg", "8-06.jpg", "8-07.jpg", "8-08.jpg", "8-09.jpg", "8-10.jpg", "8-01.png", "8-02.png", "8-03.png", "8-04.png", "8-05.png", "8-06.png", "8-07.png", "8-08.png", "8-09.png", "8-10.png"],
-                ["9-01.jpg", "9-02.jpg", "9-03.jpg", "9-04.jpg", "9-05.jpg", "9-06.jpg", "9-07.jpg", "9-08.jpg", "9-09.jpg", "9-10.jpg", "9-01.png", "9-02.png", "9-03.png", "9-04.png", "9-05.png", "9-06.png", "9-07.png", "9-08.png", "9-09.png", "9-10.png"]
+                ["0-01.png", "0-02.png", "0-03.png", "0-04.png", "0-05.png", "0-06.png", "0-07.png", "0-08.png", "0-09.png", "0-10.png"],
+                ["1-01.png", "1-02.png", "1-03.png", "1-04.png", "1-05.png", "1-06.png", "1-07.png", "1-08.png", "1-09.png", "1-10.png"],
+                ["2-01.png", "2-02.png", "2-03.png", "2-04.png", "2-05.png", "2-06.png", "2-07.png", "2-08.png", "2-09.png", "2-10.png"],
+                ["3-01.png", "3-02.png", "3-03.png", "3-04.png", "3-05.png", "3-06.png", "3-07.png", "3-08.png", "3-09.png", "3-10.png"],
+                ["4-01.png", "4-02.png", "4-03.png", "4-04.png", "4-05.png", "4-06.png", "4-07.png", "4-08.png", "4-09.png", "4-10.png"],
+                ["5-01.png", "5-02.png", "5-03.png", "5-04.png", "5-05.png", "5-06.png", "5-07.png", "5-08.png", "5-09.png", "5-10.png"],
+                ["6-01.png", "6-02.png", "6-03.png", "6-04.png", "6-05.png", "6-06.png", "6-07.png", "6-08.png", "6-09.png", "6-10.png"],
+                ["7-01.png", "7-02.png", "7-03.png", "7-04.png", "7-05.png", "7-06.png", "7-07.png", "7-08.png", "7-09.png", "7-10.png"],
+                ["8-01.png", "8-02.png", "8-03.png", "8-04.png", "8-05.png", "8-06.png", "8-07.png", "8-08.png", "8-09.png", "8-10.png"],
+                ["9-01.png", "9-02.png", "9-03.png", "9-04.png", "9-05.png", "9-06.png", "9-07.png", "9-08.png", "9-09.png", "9-10.png"]
             ];
             this.output = document.querySelector("#output");
             this.buildDrawingBoard();
@@ -89,8 +89,8 @@
                 str += "\n";
             }
             // this.output.value = str;
-            this.output.value = JSON.stringify(data_arr);
-            // this.output.value = JSON.stringify(target_arr);
+            // this.output.value = JSON.stringify(data_arr);
+            this.output.value = JSON.stringify(target_arr);
         }
     }
 
