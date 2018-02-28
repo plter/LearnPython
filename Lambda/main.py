cb2f8128338088cc551a96bd5de4a3a8a18b2043
yunp <xtiqin@163.com>
2018-02-28 10:07:32 +0800

    正太分布

---
+++ Lambda/main.py
@@ -0,0 +1,3 @@
+f = lambda a, b: a + b
+
+print(f(2, 3))
---
+++ NumpyRand/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.random.randn(10, 10)
+b = np.random.rand(200)
+
+print(a)
+# print(b)
+
+# print(np.random.rand(1000))
+
+
+num_count = 2000
+x = np.random.randn(num_count)
+# y = np.full(num_count, 0, dtype=np.float32)
+y = np.random.rand(num_count) * 5
+print(y)
+
+plt.plot(x, y, '.')
+plt.show()
